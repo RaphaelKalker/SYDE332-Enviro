aa00e1285fd6df109ac075fb28fa89de6b348643
Raphael Kalker <raphael.kalker@gmail.com>
2015-03-25 20:35:17 -0400

    use properties for constants

--- Infection.m
+++ Infection.m
@@ -7,25 +7,31 @@ classdef Infection
         R = 1; %recovered
         S0 = .99
         I0 = .1;
-        aR = 0;
+        R0 = 0;
         t0 = 0;
         tf = 100;
     end
     
     methods (Static)
         function plotODE45()
-            [T,Y] =ode45(@solve_SIR,[0 50],[99,1,10]);
+%             a = Infection.a; 
+%             b = Infection.b; S0 = Infection.S0
+            [T,Y] = ode45(@solve_SIR,[t0 tf],[...
+                Infection.S0...,
+                Infection.I0...,
+                Infection.R0]);
+
             plot(T,Y(:,1),T,Y(:,2),T,Y(:,3));
-            legend('S','I','R');
+            legend('Susceptible','Infected','Recovered');
         end
     end
     
 end
 
-
 % y(1) = S
 % y(2) = I
 % y(3) = R
+% y(4) = E
 function f = solve_SIR(t,y)
     a = Infect.a; b = Infect.b;
     f = zeros(3,1);
