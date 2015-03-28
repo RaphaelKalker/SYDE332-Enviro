cee7e535dc6ba5ee58ea80960b50e61d9e65e590
Tim <timjj22@gmail.com>
2015-03-28 01:48:34 -0400

    Got a forward euler implementation in, prolly works, didn't test too much though

--- Infection.m
+++ Infection.m
@@ -10,7 +10,8 @@ classdef Infection
     
     methods (Static)
         function finalVals = getFinalValues(POP, S0, E0, I0, R0)
-            [T,Y] = ode45(@solve_SIR, [Infection.t0 Infection.tf], [S0/POP E0/POP I0/POP R0/POP]);
+            %[T,Y] = ode45(@solve_SIR, [Infection.t0 Infection.tf], [S0/POP E0/POP I0/POP R0/POP]);
+            [T,Y] = forwardEulerSIR([Infection.t0 Infection.tf], [S0/POP E0/POP I0/POP R0/POP]);
             Infection.plotODE(T, Y);
             finalVals = [Y(end,1)*POP, Y(end,2)*POP, Y(end,3)*POP, Y(end,4)*POP];
 
@@ -20,6 +21,7 @@ classdef Infection
             plot(T,Y(:,1),T,Y(:,2),T,Y(:,3),T,Y(:,4));
             legend('Susceptible', 'Exposed','Infected','Recovered');
         end
+        
     end
 end
 
@@ -33,6 +35,32 @@ function f = solve_SIR(t,y)
     f(2) = a*y(1)*y(3) - c*y(2); %E' = bSI - cE
     f(3) = c*y(2)-b*y(3); %I' = cE - bI
     f(4) = b*y(3); %R = bI
+end
+
+%just returns final values
+function [T,Y] = forwardEulerSIR(timeParams, IC)
+%forward euler for system at 0.01 timestep
+    timestep = 0.0001;
+    T = (timeParams(1):timestep:timeParams(2))';
+    Y = zeros(length(T),4);
+    
+    Y(1,1) = IC(1);
+    Y(1,2) = IC(2);
+    Y(1,3) = IC(3);
+    Y(1,4) = IC(4);
+    
+    a = Infection.a; 
+    b = Infection.b;
+    c = Infection.c;
+    
+    for i = 2:length(T)
+        Y(i,1) = Y(i - 1,1) + timestep*(-a*Y(i - 1,1)*Y(i - 1,3)); %S' = -bSI
+        Y(i,2) = Y(i - 1,2) + timestep*(a*Y(i - 1,1)*Y(i - 1,3) - c*Y(i - 1,2)); %E' = bSI - cE
+        Y(i,3) = Y(i - 1,3) + timestep*(c*Y(i - 1,2)-b*Y(i - 1,3)); %I' = cE - bI
+        Y(i,4) = Y(i - 1,4) + timestep*(b*Y(i - 1,3)); %R = bI
+    end
+    
+    Q = [T,Y];
 end
 
 
