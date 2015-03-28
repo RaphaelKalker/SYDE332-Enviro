90a18cdac234045b9f9874939ecc489aada0de9e
Tim <timjj22@gmail.com>
2015-03-27 22:32:45 -0400

    Fixed ODE thing

--- Infection.m
+++ Infection.m
@@ -1,38 +1,37 @@
 classdef Infection
     properties (Constant)
-        a = .5; %inflection rate
-        b = .3; %recovery rate
-        S = 1; % Susceptible
-        I = 1; %infected
-        R = 1; %recovered
-        %         S0 = .99
-        %         I0 = .1;
-        %         R0 = 0;
+        a = 0.55; %infection rate
+        c = 0.1; %1/c = average latent infection time
+        b = .10; %recovery rate
         E0 = .1;
         t0 = 0;
-        tf = 5;
+        tf = 3;
     end
     
     methods (Static)
-        function finalVals = getFinalValues(S0, E0, I0, R0)
-            [T,Y] = ode45(@solve_SIR, [Infection.t0 Infection.tf], [S0 I0 R0]);
-            Infection.plotODE(T, Y, S0, E0, I0, R0);
-            finalVals = [Y(end,1), 0, Y(end,2), Y(end,3)];
+        function finalVals = getFinalValues(POP, S0, E0, I0, R0)
+            [T,Y] = ode45(@solve_SIR, [Infection.t0 Infection.tf], [S0/POP E0/POP I0/POP R0/POP]);
+            Infection.plotODE(T, Y);
+            finalVals = [Y(end,1)*POP, Y(end,2)*POP, Y(end,3)*POP, Y(end,4)*POP];
         end
         
-        function plotODE(T, Y, S0, E0, I0, R0)
-            plot(T,Y(:,1),T,Y(:,2),T,Y(:,3));
-            legend('Susceptible','Infected','Recovered');
+        function plotODE(T, Y)
+            plot(T,Y(:,1),T,Y(:,2),T,Y(:,3),T,Y(:,4));
+            legend('Susceptible', 'Exposed','Infected','Recovered');
         end
     end
 end
 
 function f = solve_SIR(t,y)
-    a = Infection.a; b = Infection.b;
-    f = zeros(3,1);
-    f(1) = -a*y(1)*y(2); %S
-    f(2) = a*y(1)*y(2)-b*y(2); %I
-    f(3) = b*y(2); %R
+    a = Infection.a; 
+    b = Infection.b;
+    c = Infection.c;
+    
+    f = zeros(4,1);
+    f(1) = -a*y(1)*y(3); %S' = -bSI
+    f(2) = a*y(1)*y(3) - c*y(2); %E' = bSI - cE
+    f(3) = c*y(2)-b*y(3); %I' = cE - bI
+    f(4) = b*y(3); %R = bI
 end
 
 
--- MakeItRain.m
+++ MakeItRain.m
@@ -4,23 +4,27 @@ GRID = GridCell(10,10);
 
 for j = 1:GRID.w
     for i = 1:GRID.h
-        GRID.setValues([i,j], i, j, 1, 1, 1);
+        GRID.setValues([i,j], 60, 15, 15, 0, 10);
     end
 end
 
 %update all of the SIR models with the ODE45 solver
-for j = 1:GRID.w
-    for i = 1:GRID.h
-        
-        vals = GRID.getValues([i,j]);
-        A = Infection.getFinalValues(vals(2), vals(3), vals(4), vals(5));
-        vals(2) = A(1);
-        vals(3) = A(2);
-        vals(4) = A(3);
-        vals(5) = A(4);
-        GRID.setValues([i,j], vals(2), vals(3), vals(4), vals(5), vals(6));
-    end
-end
+% for j = 1:GRID.w
+%     for i = 1:GRID.h
+%         
+%         vals = GRID.getValues([i,j]);
+%         A = Infection.getFinalValues(vals(2), vals(3), vals(4), vals(5));
+%         vals(2) = A(1);
+%         vals(3) = A(2);
+%         vals(4) = A(3);
+%         vals(5) = A(4);
+%         GRID.setValues([i,j], vals(2), vals(3), vals(4), vals(5), vals(6));
+%     end
+% end
+
+%single test case
+vals = GRID.getValues([i,j]);
+A = Infection.getFinalValues(vals(1), vals(2), vals(3), vals(4), vals(5));
 
 %Test swap cells function
 NewCs = SEIR.swapCells([1,2,3,4,5,6]', [1,2,3,4,5,6]')
